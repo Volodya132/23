bc2bf76dc0d6bf58411b879a01aac4745e9f52c5
Volodyanltu <volodymyrolshanskyi@nltu.lviv.ua>
2023-05-13 11:59:37 +0300

    version 2

--- test.py
+++
@@ -1,4 +0,0 @@
-import pyautogui as pag
-from time import*
-sleep(2)
-[[pag.write(i) if (i!='') else pag.press('enter') for i in ['RUSSIAN FUCKING MILITARY SHIP GO NAHUI!!!','','SLAVA UKRAINI','','PUTLER HUILO','','FUCK RUSSIAN','','RUSSIAN NAZI','','SUCK','','ZDOHNIT PADLY''',]] for j in range(10)]
